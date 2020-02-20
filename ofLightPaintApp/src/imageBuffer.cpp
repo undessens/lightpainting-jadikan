1c104d4a1f8e49b6c0bc1029259b7acf16b71913
UnDesSens <assoundessens@gmail.com>
2020-02-20 15:14:30 +0100

    ofblendmode to screen mode

--- ofLightPaintApp/src/imageBuffer.cpp
+++ ofLightPaintApp/src/imageBuffer.cpp
@@ -59,7 +59,7 @@ void ImageBuffer::update(ofFbo* input){
 
 
     fbo.begin();
-    ofEnableBlendMode(OF_BLENDMODE_RESET);
+    ofEnableBlendMode(OF_BLENDMODE_SCREEN);
     ofSetColor(opacityAtDraw,opacityAtDraw,opacityAtDraw);
     input->draw(0, 0);
     ofDisableBlendMode();
